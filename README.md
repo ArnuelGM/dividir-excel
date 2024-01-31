d1ff8696c36e57970935457110ea2d9801590fd4
Arnuel Gutierrez Menco <argume31@hotmail.com>
2024-01-31 15:01:18 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Divide Excel
+
+Divide an excel sheet in other excel files of `chunk_size` rows, or create others sheets if `divide_in_sheets` param is true.
+
+
+## USAGE
+
+```shell
+$ src/index.php <filename> <chunk_size> [<divide_in_sheets>]
+```
---
+++ src/index.php
@@ -0,0 +1,50 @@
+#!/usr/bin/env php
+<?php
+
+
+use PhpOffice\PhpSpreadsheet\Reader\Xlsx as Reader;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx as Writer;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+
+require_once '../vendor/autoload.php';
+
+$args = $_SERVER['argv'];
+
+$inputFileName = $args[1];
+
+if(!file_exists($inputFileName)) {
+    echo "Error:  File not found.\n";
+    exit(1);
+}
+
+$spreadsheet = (new Reader)->load($inputFileName);
+
+$datos = $spreadsheet->getSheet(0)->toArray();
+
+$chunks = [];
+
+$chunkSize = $args[2] ?: 1000;
+
+$chunks = array_chunk($datos, $chunkSize);
+
+$divideInSheets = count($args) > 3 ? !!$args[3] : false;
+
+foreach($chunks as $index => $chunk) {
+
+    if($divideInSheets) {
+        $hoja = $spreadsheet->createSheet();
+        $hoja->fromArray($chunk);
+    }
+    else {
+        $excel = new Spreadsheet();
+        $hoja = $excel->getSheet(0);
+        $hoja->fromArray($chunk);
+        $writer = new Writer($excel);
+        $writer->save("{$inputFileName}_{$index}.xlsx");
+    }
+}
+
+if($divideInSheets) {
+    $writer = new Writer($spreadsheet);
+    $writer->save($inputFileName);
+}
